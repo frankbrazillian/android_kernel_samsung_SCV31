d92558f6e9317c604bd16a28ebdb598559fbad9f
Frankbrazillian <frankbrazillian@gmail.com>
2018-06-24 09:56:24 +0100

    Source code of SCV31 NN Kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 23 16:40:00 WEST 2018"
+#define LINUX_COMPILE_BY "frank"
+#define LINUX_COMPILE_HOST "brazillian"
+#define LINUX_COMPILER "gcc version 7.2.1 20170913 (Linaro GCC 7.2-2017.10~dev) "
